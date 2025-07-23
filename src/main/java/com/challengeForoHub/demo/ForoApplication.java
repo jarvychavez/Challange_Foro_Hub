4c7b17aa4db93b68514ab3a773c585734751a06b
jarvychavez <jav.chavez94@gmail.com>
2025-07-22 19:30:36 -0600

    hello world

--- src/main/java/com/challengeForoHub/demo/ForoApplication.java
+++ src/main/java/com/challengeForoHub/demo/ForoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ForoApplication.class, args);
 	}
 

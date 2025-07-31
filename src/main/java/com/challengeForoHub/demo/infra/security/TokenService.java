520087bb086906327e4cf806d853697022c4240a
jarvychavez <jav.chavez94@gmail.com>
2025-07-31 13:08:01 -0600

    conseguimos poner que usuario es el que esta llamando y generando el token correcto

--- src/main/java/com/challengeForoHub/demo/infra/security/TokenService.java
+++ src/main/java/com/challengeForoHub/demo/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.challengeForoHub.demo.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.challengeForoHub.demo.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,5 +35,19 @@ public class TokenService {
     private Instant fechaExpiracion() {
         ZoneId zonaGuadalajara = ZoneId.of("America/Mexico_City");
         return LocalDateTime.now().plusHours(2).atZone(zonaGuadalajara).toInstant();
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Foro.hub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT Invalido o expirado....!");
+        }
     }
 }
